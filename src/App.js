12a87aaa27cbb8e14f1a48ba5ce3441d9ffecd8a
Renda Nyamande <rendanyamande44@gmail.com>
2022-09-09 04:13:48 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+  const chords = ["1", "2", "3", "4", "5", "6", "7",
+                 "1/3", "2/4", "3/5", "4/6", "5/7", "6/1", "7/2",
+                 "1/5", "2/6", "3/7", "4/1", "5/2", "6/3", "7/4"];
+  const [num, setNum] = useState(0);
+
+  // add side effect to component
+  useEffect(() => {
+    // create interval
+    const interval = setInterval(
+      // set number every 5s
+      () => setNum(Math.floor(Math.random() * chords.length+ 1)),
+      3000
+    );
+
+    // clean up interval on unmount
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{chords[num-1]}</h1>
     </div>
   );
 }
